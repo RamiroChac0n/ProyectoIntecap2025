c422919f13e3705ef3c2715db6b4003121a1b2b2
vgelzer <elzervillela@gmail.com>
2025-12-13 10:31:03 -0600

    refactor(app): funcion para crear app de flask

--- app.py
+++ app.py
@@ -3,23 +3,25 @@ from flask import Flask
 from src.comun.utilidades import db, api, ma, jwt
 from src.rutas.rutas import RutasGeneral
 
-app = Flask(__name__)
+
+def crear_aplicacion():
+    app = Flask(__name__)
 
 
 
 
-app.config.from_object("configuracion.Configuracion")
+    app.config.from_object("configuracion.Configuracion")
 
-#iniciar las rutas
-RutasGeneral(api)
-
-
-db.init_app(app)
-api.init_app(app)
-ma.init_app(app)
-jwt.init_app(app)
+    #iniciar las rutas
+    RutasGeneral(api)
 
 
-    
-if __name__ == '__main__':
-    app.run(debug=True)
+    db.init_app(app)
+    api.init_app(app)
+    ma.init_app(app)
+    jwt.init_app(app)
+
+
+    return app
+
+app = crear_aplicacion()
